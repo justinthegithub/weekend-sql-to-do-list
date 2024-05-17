b5aa2f8893613b30c39ebfd616b1aa994d5dfad4
Justin Kahn <jdavidkahn@gmail.com>
2024-05-17 15:38:47 -0500

    tested put route

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,31 +1,47 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
-router.get('/', async (req, res) => {
-    try {
-      const result = await pool.query('SELECT * FROM todos');
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server error');
-    }
-  });
 
-  router.post('/', async (req, res) => {
-    const { text } = req.body;
-    try {
-      const result = await pool.query(
-        'INSERT INTO todos (text) VALUES ($1) RETURNING *',
-        [text]
-      );
-      res.json(result.rows[0]);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Server error');
-    }
-  });
+router.get('/', (req, res) => {
+  pool.query('SELECT * FROM todos')
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 
+router.post('/', (req, res) => {
+  const { text } = req.body;
+  pool.query('INSERT INTO todos (text) VALUES ($1) RETURNING *', [text])
+    .then((result) => {
+      console.log("Hello from router.post", req.body)
+      res.send(result.rows[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  pool.query('UPDATE todos SET completed = TRUE WHERE id = $1 RETURNING *', [id])
+    .then((result) => {
+      res.send(result.rows[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 
-  
+
+
+
+
+
 module.exports = router;
