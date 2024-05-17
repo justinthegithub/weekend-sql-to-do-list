5b0f7ef1055131e820e016611fcf5f934523fd69
Justin Kahn <jdavidkahn@gmail.com>
2024-05-17 15:12:30 -0500

    router.post added

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,6 +1,31 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
-router.get('/', (req, res) => {
-    res.send('Hello, world!');
+router.get('/', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM todos');
+      res.json(result.rows);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server error');
+    }
   });
+
+  router.post('/', async (req, res) => {
+    const { text } = req.body;
+    try {
+      const result = await pool.query(
+        'INSERT INTO todos (text) VALUES ($1) RETURNING *',
+        [text]
+      );
+      res.json(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server error');
+    }
+  });
+
+
+
+
+  
 module.exports = router;
