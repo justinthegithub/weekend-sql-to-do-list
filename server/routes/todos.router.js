6d55c9a69714fbeec069ed469ae2c2c5519e7280
Justin Kahn <jdavidkahn@gmail.com>
2024-05-17 08:42:10 -0500

    deleted repository starting over

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
-
-
-
-
+router.get('/', (req, res) => {
+    res.send('Hello, world!');
+  });
 module.exports = router;
