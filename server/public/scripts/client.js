ea58e8870dd81c002cfb87db86a70453e85a325a
Justin Kahn <jdavidkahn@gmail.com>
2024-05-18 09:20:31 -0500

    loadToDos test

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,31 @@
 console.log('JS is sourced!');
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('Hello from client.js. DOM loaded');
+   
+    const todoInput = document.querySelector('#todoInput');
+    const submitBtn = document.querySelector('#theSubmitButton');
+    const todoList =document.querySelector('#todoList');
+    console.log('querySelector check',todoInput,theSubmitButton, todoList);
+
+   
+    theSubmitButton.addEventListener('click', function(){
+        console.log('Button Clicked', todoInput.value)
+    })
+
+    function loadToDos() {
+        axios({
+            method: 'get',
+            url: '/todos'
+        })
+        .then(function(response){
+            console.log(response.data)
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+    }
+    loadToDos();
+
+
+});
